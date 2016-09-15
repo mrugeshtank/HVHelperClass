1646c1e52aa8d8e20092155569ef77197546affd
eheuristic <eheuristic@eheuristics-mac-pro.local>
2016-09-15 12:15:16 +0530

    Initial Commit

---
+++ HVHelperClass/HVHelperClass.h
@@ -0,0 +1,13 @@
+//
+//  HVHelperClass.h
+//  HVHelperClass
+//
+//  Created by eheuristic on 15/September/2016.
+//  Copyright © 2016 eheuristic. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HVHelperClass : NSObject
+
+@end
---
+++ HVHelperClass/HVHelperClass.m
@@ -0,0 +1,13 @@
+//
+//  HVHelperClass.m
+//  HVHelperClass
+//
+//  Created by eheuristic on 15/September/2016.
+//  Copyright © 2016 eheuristic. All rights reserved.
+//
+
+#import "HVHelperClass.h"
+
+@implementation HVHelperClass
+
+@end
